8790b93973c94ceb2cdf85bbca1672982b4e8bec
Muhammad Azeem <chaudhrryazeem@gmail.com>
2025-11-28 00:35:06 +0100

    new source added

--- src/export-to-docs.ps1
+++
@@ -1,8 +0,0 @@
-# PowerShell script to move static export to docs folder
-
-# Remove the old docs folder if it exists
-if (Test-Path ../docs) { Remove-Item ../docs -Recurse -Force }
-# Recreate the docs folder
-New-Item -ItemType Directory -Path ../docs | Out-Null
-# Copy all contents from out to docs
-Copy-Item -Path ./out/* -Destination ../docs -Recurse
--- src/src/pages/index.jsx
+++
@@ -1,3 +0,0 @@
-import ComingSoon from "./coming-soon";
-
-export default ComingSoon;
